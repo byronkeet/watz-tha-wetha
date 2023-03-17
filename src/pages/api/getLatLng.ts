3b8467cb0776a16f9af0d816401a06b766ffed61
Byron Keet <keetbis@gmail.com>
2023-03-17 07:21:18 +0200

    Added getLatLng route to retrieve lat and lng from address with position stack api

---
+++ src/pages/api/getLatLng.ts
@@ -0,0 +1,55 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+interface GetLatLngBody {
+	address: string;
+}
+
+interface GetAddressData {
+	data: {
+		latitude: number,
+		longitude: number,
+		type: string,
+		name: string,
+		number: string,
+		postal_code: string,
+		street: string,
+		confidence: string,
+		region: string,
+		region_code: string,
+		county: string,
+		locality: string,
+		administrative_area: string,
+		neighbourhood: string,
+		country: string,
+		country_code: string,
+		continent: string,
+		label: string,
+	}[]
+}
+
+const getLatLng = async (req: NextApiRequest, res: NextApiResponse) => {
+	const { address } = req.body as GetLatLngBody;
+
+	const options = {
+		access_key: process.env.POSITIONSTACK_API_KEY as string,
+		query: address,
+	};
+
+	const queryUrl = 'http://api.positionstack.com/v1/forward?' + new URLSearchParams(options).toString();
+		
+		console.log(queryUrl);
+
+	try {
+		const response = await fetch(queryUrl);
+		const data = await response.json() as GetAddressData;
+
+		res.status(200);
+		res.json(data.data[0]);
+	} catch (err) {
+		console.error(err);
+		res.status(500);
+		res.json({ err: 'failed'})
+	}
+}
+
+export default getLatLng;
