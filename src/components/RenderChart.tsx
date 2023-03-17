e29b43f9337add6223ebdf46655e4086df88ca5b
Byron Keet <keetbis@gmail.com>
2023-03-17 11:08:12 +0200

    Moved legend to top. Added button to toggle chart type. Changed chart colors

--- src/components/RenderChart.tsx
+++ src/components/RenderChart.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import dynamic from 'next/dynamic'
 
 const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
@@ -7,47 +7,68 @@ import type { Temperatures } from './FormInputs';
 
 interface ChartData {
 	chartData: Temperatures;
-  }
+}
+
 
 const RenderChart = ({chartData}: ChartData) => {
+	const [chartType, setChartType] = useState("line")
 	const options = {
 		chart: {
 			id: "watz-tha-weatha",
 			width: '700px'
-		  },
-		  xaxis: {
+		},
+		xaxis: {
 			categories: chartData.time
-		  },
+		},
+		legend: {
+			position: 'top',
+		},
+		colors: ['#ffd203', '#7bdcff'],
+		dataLabels: {
+			enabled: true,	
 		}
+	}
 
 
 
-		const series = [
-			{
-				name: "max-temp",
-				data: chartData.temperature_2m_max
-			},
-			{
-			  name: "min-temp",
-			  data: chartData.temperature_2m_min
-			},
-		  ]
+	const series = [
+		{
+			name: "max-temp",
+			data: chartData.temperature_2m_max
+		},
+		{
+			name: "min-temp",
+			data: chartData.temperature_2m_min
+		},
+	]
+
+	const handledToggleChart = () => {
+		setChartType(chartType === 'bar' ? "line" : "bar")
+	}
 
 	return (
 		<div className="app lg:w-2/3 w-full">
-		  <div className="row">
-			<div className="mixed-chart">
-			{(typeof window !== 'undefined') &&
-			  <Chart
-				options={options}
-				series={series}
-				type="line"
-			  />
-			}
+			<div className="row">
+				<div className="mixed-chart">
+					<p className="text-xs text-center text-white mb-5">Click on legend to select and de-select data</p>
+				{(typeof window !== 'undefined') &&
+					<Chart
+					options={options}
+					series={series}
+					type={chartType}
+					/>
+				}
+				</div>
+				<div className="chart-choice text-center">
+					<button
+						className="bg-[#ff6b6b] text-white px-4 py-2 rounded-lg" onClick={handledToggleChart}
+					>
+						Swith to {chartType === 'bar' ? 'Line' : 'Bar'} Chart
+					</button>
+				</div>
 			</div>
-		  </div>
 		</div>
-  )
+	)
 }
 
 export default RenderChart
