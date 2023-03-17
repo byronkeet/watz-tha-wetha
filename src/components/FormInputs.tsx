021b0106006cdf4c87676d2fdd9e01b0f01f41d2
Byron Keet <keetbis@gmail.com>
2023-03-17 10:26:53 +0200

    Removed console log

--- src/components/FormInputs.tsx
+++ src/components/FormInputs.tsx
@@ -37,7 +37,7 @@ const FormInputs = ({setLoading, setTemperatures, loading}: FormInputsProps) => 
 			setLoading(false);
 			return;
 		}
-		console.log(address)
+
 		request<Address>(`http://localhost:3000/api/getLatLng`, {
 			method: 'POST',
 			body: JSON.stringify({ address: address })
